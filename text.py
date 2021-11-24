e40e9b3999cffdfa410f6f81277123cce1bbe629
Michael Lechowicz <ichal.lechowicz@gmail.com>
2021-11-24 14:46:00 +0100

    Create module text

--- text.py
+++ text.py
@@ -1,10 +1,10 @@
+# YOUR CODE HERE
 def left(txt:str, n=80):
     for i in range(int(len(txt)/ n) + 1):
         print(txt[i * n:(i+1)*n])
 
 def right(txt:str, n=80):
     m = int(len(txt)/ n ) +1
-    print(m)
     for i in range(m):
         if i == m -1:
             print((n - len(txt[i*n:(i+1)*n]))* ' ' + txt[i*n:(i +1) *n])
@@ -14,10 +14,19 @@ def right(txt:str, n=80):
 def center(txt:str, n=80):
     m = int(len(txt)/ n ) +1
     for i in range(m):
-        if i == m +1:
+        if i == m -1:
             space = int((n - len(txt[i*n:(i+1)*n]))/2)
             print(space * ' ', txt[i*n:(i+1)*n],space*' ')
         else:
             print(txt[i*n:(i+1)* n])
 #raise NotImplementedError()
-right("123456789ABCDEFGHIJKLMNOPRSTUWXYZ", 10)
+if __name__ == "__main__":
+    #kod uruchomiony jako program
+    pass
+else:
+    #plik uruchomiony jako moduł
+    print('Moduł "text" definiuje funkcje center, left, right i służy do:')
+    left('Wypisywania tekstu wyrównanego do lewej,', 80)
+    right("do prawej", 80)
+    center("lub wycentrowanego.", 80)
+    left("Dodatkowo ten moduł zawiera: " + str(dir()), 80)
