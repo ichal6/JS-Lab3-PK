49620df778cd9b67854e6c437d6813c6e8860675
Michael Lechowicz <ichal.lechowicz@gmail.com>
2021-11-25 13:55:44 +0100

    Implement zad5a

---
+++ z5_en.py
@@ -0,0 +1,6 @@
+lang={}
+lang['hello'] = 'Hello, stranger!'
+lang['info'] = 'Write any number, I will calculate root for you: '
+lang['error'] = 'This data are incorrect'
+lang['bye'] = 'Goodbye !'
+
---
+++ z5_pl.py
@@ -0,0 +1,6 @@
+lang={}
+lang['hello'] = 'Witaj, nieznajomy!'
+lang['info'] = 'Podaj dowolną liczbę, policzę dla Ciebie pierwiastek : '
+lang['error'] = 'Podane dane są nieprawidłowe'
+lang['bye'] = 'Żegnaj !'
+
